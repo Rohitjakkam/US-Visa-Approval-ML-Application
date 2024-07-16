341451c1eee9883ceeef13918ac4ea49c4e21424
Rohitjakkam <rohitjakkam@gmail.com>
2024-07-16 11:57:17 +0530

    update setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='us-visa-approval',
+    version='0.0.1',
+    author='rohitjakkam',
+    author_email='rohitjakkam@gmail.com',
+    description='US Visa Approval Prediction',
+    packages=find_packages()
+)
