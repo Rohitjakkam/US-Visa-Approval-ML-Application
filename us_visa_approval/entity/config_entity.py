94908a84c2a4d6ab1fe1d7e287a0baa6d2acf891
Rohitjakkam <rohitjakkam@gmail.com>
2024-07-19 01:34:09 +0530

    update config entity

--- us_visa_approval/entity/config_entity.py
+++ us_visa_approval/entity/config_entity.py
@@ -45,7 +45,7 @@ class TrainingPipelineConfig:
     pipeline_name: str = PIPELINE_NAME
     artifact_dir: str = os.path.join(ARTIFACT_DIR, TIMESTAMP)
     timestamp: str = TIMESTAMP
-    training_pipeline_config: TrainingPipelineConfig = TrainingPipelineConfig()
+training_pipeline_config: TrainingPipelineConfig = TrainingPipelineConfig()
 
 
 @dataclass
